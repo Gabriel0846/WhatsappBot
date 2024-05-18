afee6bfbfbbf75944036135b3bb32a3dc498f8c6
Gabriel Lopes <carnagesagas@gmail.com>
2024-05-18 10:40:01 -0300

    utilizado pyautogui para automação de envio após acessar o link e criado teste que retorna erros em caso de falha

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ import openpyxl
 from urllib.parse import quote
 import webbrowser
 from time import sleep
+import pyautogui
 
 webbrowser.open('https://web.whatsapp.com/')
-sleep(30)
+sleep(0)
 
 workbook = openpyxl.load_workbook("clientes.xlsx")
 pagina_clientes = workbook["Planilha1"]
@@ -18,5 +19,15 @@ for linha in pagina_clientes.iter_rows(min_row=2):
 
     link_mensagem_whatsapp = f'https://web.whatsapp.com/send?phone={telefone}&text={quote(mensagem)}'
     webbrowser.open(link_mensagem_whatsapp)
-
-    input()
+    sleep(10)
+    try:
+        seta = pyautogui.locateCenterOnScreen('seta.png')
+        sleep(2)
+        pyautogui.click(seta[0], seta[1])
+        sleep(2)
+        pyautogui.hotkey('ctrl', 'w')
+        sleep(2)
+    except:
+        print(f'Não foi possivel enviar a mensagem para {nome}')
+        with open('erros.csv', 'a', newline='', encoding='utf-8') as arquivo:
+            arquivo.write(f'{nome}, {telefone}')
